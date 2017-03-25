b301e925b68a5192a247965c62501794af88f9f0
Vartika Jayaswal <vartikajayaswal@yahoo.com>
2017-03-25 23:53:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+var app = angular.module('randomForm',[]);
+
+app.controller("formController",['$scope','$http', function($scope,$http){
+//    $scope.key = {};
+//    $http.get('https://randomform.herokuapp.com/').then(function(response){
+//        $scope.key.val = response.data.data;
+//        var str = JSON.stringify(response.data.data, null, 2);
+//        $scope.key.val.field = str;
+//    });
+    debugger;
+    $scope.click= function(){
+        $scope.key = {};
+    $http.get('https://randomform.herokuapp.com/').then(function(response){
+        $scope.key.val = response.data.data;
+        var str = JSON.stringify(response.data.data, null, 2);
+        $scope.key.val.field = str;
+    });
+    }
+        
+    $scope.submit = function(){
+        var req = {
+            method: 'POST',
+            url: 'https://randomform.herokuapp.com/submit',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: $scope.key.val.field
+        }
+        $http(req).then(function(response){
+            var retStr = JSON.stringify(response.data, null, 2);
+            $scope.key.capture = retStr;
+            $http.get('https://randomform.herokuapp.com/').then(function(response){
+                $scope.key.val = response.data.data;
+                var str = JSON.stringify(response.data.data, null, 2);
+                $scope.key.val.field = str;
+            });
+        });
+    }
+
+}]);
